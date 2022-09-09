6a2339b2106a7749eb79f4003e43358baaba0c74
Roland Erk <roland.erk@conplement.de>
2022-09-09 14:08:49 +0200

    Fix storing of timestamp for NIST mirror files in modification time since this cannot be set on CIFS

--- src/main/java/org/dependencytrack/tasks/NistMirrorTask.java
+++ src/main/java/org/dependencytrack/tasks/NistMirrorTask.java
@@ -43,6 +43,9 @@ import org.dependencytrack.persistence.QueryManager;
 
 import java.io.Closeable;
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -190,7 +193,15 @@ public class NistMirrorTask implements LoggableSubscriber {
             filename = filename.substring(filename.lastIndexOf('/') + 1);
             file = new File(outputDir, filename).getAbsoluteFile();
             if (file.exists()) {
-                if (System.currentTimeMillis() < ((86400000 * 5) + file.lastModified())) {
+                long modificationTime = 0;
+                File timestampFile = new File(outputDir, filename + ".ts");
+                if(timestampFile.exists()) {
+                    BufferedReader tsBufReader = new BufferedReader(new FileReader(timestampFile));
+                    String text = tsBufReader.readLine();
+                    modificationTime = Integer.parseInt(text);
+                }
+
+                if (System.currentTimeMillis() < ((86400000 * 5) + modificationTime)) {
                     if (ResourceType.CVE_YEAR_DATA == resourceType) {
                         LOGGER.info("Retrieval of " + filename + " not necessary. Will use modified feed for updates.");
                         return;
@@ -221,7 +232,10 @@ public class NistMirrorTask implements LoggableSubscriber {
                         Files.move(temp.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
                         if (ResourceType.CVE_YEAR_DATA == resourceType || ResourceType.CVE_MODIFIED_DATA == resourceType) {
                             // Sets the last modified date to 0. Upon a successful parse, it will be set back to its original date.
-                            file.setLastModified(0);
+                            File timestampFile = new File(outputDir, filename + ".ts");
+                            FileWriter writer = new FileWriter(timestampFile);
+                            writer.write(Integer.toString(0));
+                            writer.close();
                         }
                         if (file.getName().endsWith(".gz")) {
                             uncompress(file, resourceType);
@@ -286,7 +300,12 @@ public class NistMirrorTask implements LoggableSubscriber {
             if (ResourceType.CVE_YEAR_DATA == resourceType || ResourceType.CVE_MODIFIED_DATA == resourceType) {
                 final NvdParser parser = new NvdParser();
                 parser.parse(uncompressedFile);
-                file.setLastModified(start);
+
+                File timestampFile = new File( file.getAbsolutePath() + ".ts");
+                FileWriter writer = new FileWriter(timestampFile);
+                writer.write(Integer.toString(start));
+                writer.close();
+
             }
             final long end = System.currentTimeMillis();
             metricParseTime += end - start;
