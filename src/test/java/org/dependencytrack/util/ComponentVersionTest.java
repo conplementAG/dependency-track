12e7a162c9b75f291b4497f1b1114871cd1adec7
Alexander Zenker <Alexander.Zenker@conplement.de>
2022-10-27 13:58:01 +0200

    Added Version Tests

--- src/test/java/org/dependencytrack/util/ComponentVersionTest.java
+++ src/test/java/org/dependencytrack/util/ComponentVersionTest.java
@@ -16,4 +16,55 @@ public class ComponentVersionTest {
         Assert.assertEquals("1", parts.get(1));
         Assert.assertEquals("1j", parts.get(2));
     }
+
+    @Test
+    public void testParseAlpineVersion() {
+        ComponentVersion version = new ComponentVersion("1.2.11-r3");
+
+        List<String> parts = version.getVersionParts();
+        Assert.assertEquals(4, parts.size());
+        Assert.assertEquals("1", parts.get(0));
+        Assert.assertEquals("2", parts.get(1));
+        Assert.assertEquals("11", parts.get(2));
+        Assert.assertEquals("r3", parts.get(3));
+    }
+
+    @Test
+    public void testParseUbuntuVersion() {
+        ComponentVersion version = new ComponentVersion("1:1.2.11.dfsg-2ubuntu9");
+
+        List<String> parts = version.getVersionParts();
+        Assert.assertEquals("1", parts.get(0));
+        Assert.assertEquals("2", parts.get(1));
+        Assert.assertEquals("11", parts.get(2));
+    }
+
+    @Test
+    public void testParseUbuntu2Version() {
+        ComponentVersion version = new ComponentVersion("1.5.79ubuntu1");
+
+        List<String> parts = version.getVersionParts();
+        Assert.assertEquals("1", parts.get(0));
+        Assert.assertEquals("5", parts.get(1));
+        Assert.assertEquals("79", parts.get(2));
+    }
+
+    @Test
+    public void testParseDebianVersion() {
+        ComponentVersion version = new ComponentVersion("1:1.2.11.dfsg-2+deb11u1");
+
+        List<String> parts = version.getVersionParts();
+        Assert.assertEquals("1", parts.get(0));
+        Assert.assertEquals("2", parts.get(1));
+        Assert.assertEquals("11", parts.get(2));
+    }
+
+    @Test
+    public void testParsePlusDfsgVersion() {
+        ComponentVersion version = new ComponentVersion("1.34+dfsg-1build3");
+
+        List<String> parts = version.getVersionParts();
+        Assert.assertEquals("1", parts.get(0));
+        Assert.assertEquals("34", parts.get(1));
+    }
 }
