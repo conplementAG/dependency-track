cf6d42ebaf7117499d100fc787626c0f9ddea253
Alexander Zenker <Alexander.Zenker@conplement.de>
2022-11-01 12:00:47 +0100

    Added test to check internal analyzer

--- src/test/java/org/dependencytrack/tasks/scanners/InternalAnalysisTaskTest.java
+++ src/test/java/org/dependencytrack/tasks/scanners/InternalAnalysisTaskTest.java
@@ -47,4 +47,38 @@ public class InternalAnalysisTaskTest extends PersistenceCapableTest {
         assertThat(vulnerabilities.getList(Vulnerability.class).get(0).getVulnId()).isEqualTo("GHSA-wjm3-fq3r-5x46");
     }
 
+    @Test
+    public void testGitFalsePositives() {
+        var project = new Project();
+        project.setName("Test");
+        project = qm.createProject(project, List.of(), false);
+        var component = new Component();
+        component.setProject(project);
+        component.setName("git");
+        component.setVersion("1:2.34.1-1ubuntu1.4");
+        component.setPurl("pkg:deb/ubuntu/git@1%3A2.34.1-1ubuntu1.4?arch=amd64&distro=ubuntu-22.04");
+        component = qm.createComponent(component, false);
+
+        //cpe:2.3:a:git_project:git:*:*:*:*:*:*:*:* ( |<=2.3.9 )
+        var vulnerableSoftware = new VulnerableSoftware();
+        vulnerableSoftware.setPurlType("golang");
+        vulnerableSoftware.setPurlNamespace("github.com/tidwall");
+        vulnerableSoftware.setPurlName("gjson");
+        vulnerableSoftware.setVersionEndExcluding("2.3.9");
+        vulnerableSoftware.setVulnerable(true);
+        vulnerableSoftware = qm.persist(vulnerableSoftware);
+
+        var vulnerability = new Vulnerability();
+        vulnerability.setVulnId("CVE-2015-7545");
+        vulnerability.setSource(Vulnerability.Source.NVD);
+        vulnerability.setVulnerableSoftware(List.of(vulnerableSoftware));
+        qm.createVulnerability(vulnerability, false);
+
+        new InternalAnalysisTask().analyze(List.of(component));
+
+        final PaginatedResult vulnerabilities = qm.getVulnerabilities(component);
+        assertThat(vulnerabilities.getTotal()).isEqualTo(0);
+        //assertThat(vulnerabilities.getList(Vulnerability.class).get(0).getVulnId()).isEqualTo("CVE-2015-7545");
+    }
+
 }
